52ce7045f6d7fb8dc8dd2c8c6274df8eb15e13c3
Ranjith Kumar <ranjithjanagama9@gmail.com>
2019-05-19 19:11:15 +0530

    docker.sh

---
+++ docker.sh
@@ -0,0 +1,6 @@
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+docker run -i --name hello --network=host deepakputhraya/python-hello:v1.0
+docker run -i --name world --network=host deepakputhraya/python-world:v1.0
+docker run --name lb -v ~/nginx.conf:/etc/nginx/nginx.conf:ro --network=host -d nginx
