b8d9db41d9f2550470c1b462cf9eecbaf2a4324b
Ranjith Kumar <ranjithjanagama9@gmail.com>
2019-05-20 14:26:53 +0530

    update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,6 +1,7 @@
 sudo dpkg --configure -a
 sudo curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+sudo curl -fsSL https://github.com/Ranjith9/kubernetes-basics/blob/master/nginx.conf -o nginx.conf
 
 docker run -i --name hello --network=host deepakputhraya/python-hello:v1.0
 docker run -i --name world --network=host deepakputhraya/python-world:v1.0
