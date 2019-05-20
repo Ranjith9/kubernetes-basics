1701410da6b2680aa910196d31281117069b66ed
Ranjith Kumar <ranjithjanagama9@gmail.com>
2019-05-20 14:06:06 +0530

    commit to docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+sudo dpkg --configure -a
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
 
 docker run -i --name hello --network=host deepakputhraya/python-hello:v1.0
 docker run -i --name world --network=host deepakputhraya/python-world:v1.0
